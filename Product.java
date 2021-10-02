0b34643ecd107ccbea4330318a199578a8df664d
vivek09e <73744393+vivek09e@users.noreply.github.com>
2021-10-02 16:12:52 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,35 @@
+package com.MileStone;
+
+public class Product {
+	String sku,name,description;
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
